574f270cfe38c99434f38c59dfdc9c95986d5ab8
Luc LABBE <luc.labbe@inge.neo-soft.fr>
2014-03-07 15:50:09 +0100

    Add annotation to clear collection.

---
+++ src/main/java/fr/wayis/framework/test/runner/annotation/ClearCollection.java
@@ -0,0 +1,19 @@
+package fr.wayis.framework.test.runner.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used by the runner to clear a specific collection.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClearCollection {
+
+    /**
+     * The name of the collection to clear.
+     */
+    String value();
+}
